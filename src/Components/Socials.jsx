f5a4e1cd7914ea785f3c9ae64fa367b705a882e5
bondcab <136003551+bondcab@users.noreply.github.com>
2024-01-01 18:01:35 +0000

    Create Socials.jsx

---
+++ src/Components/Socials.jsx
@@ -0,0 +1,11 @@
+function Socials() {
+  return (
+    <div className="socialsContainer">
+      <div className="socials">
+        <p className="socialsText">Find me on</p>
+      </div>
+    </div>
+  );
+}
+
+export default Socials;
