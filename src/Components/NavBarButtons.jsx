0917e859367f8bb1dc1832a76353ae1172ca1c63
bondcab <136003551+bondcab@users.noreply.github.com>
2024-01-01 18:01:33 +0000

    Create NavBarButtons.jsx

---
+++ src/Components/NavBarButtons.jsx
@@ -0,0 +1,14 @@
+function NavBarButtons() {
+  return (
+    <div className="navButtonsContainer">
+      <div className="navButtons">
+        <button className="navbarButton">Home</button>
+        <button className="navbarButton">About</button>
+        <button className="navbarButton">Contact</button>
+        <button className="navbarButton">Work</button>
+      </div>
+    </div>
+  );
+}
+
+export default NavBarButtons;
