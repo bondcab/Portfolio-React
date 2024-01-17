45130f1499e104570b6b179df846942facffbe34
bondcab <136003551+bondcab@users.noreply.github.com>
2024-01-17 09:58:47 +0000

    Create SectionOne.jsx

---
+++ src/Components/SectionOne.jsx
@@ -0,0 +1,30 @@
+function SectionOne() {
+  return (
+    <div className="landingPageContainer">
+      <div className="sectionOneTextContainer">
+        <h1 className="sectionOneHeading">Hello World </h1>
+        <h1 className="sectionOneSlogan">
+          My name is Chris Bond. I am a web developer from the UK
+        </h1>
+        <p className="sectionOneText">Check out some of my code</p>
+
+        <button className="sectionOneButton" onClick={""} type="button">
+          Projects
+        </button>
+      </div>
+      <div className="profilePicContainer">
+        <img
+          src="portrait.jpeg"
+          alt="profile Pic"
+          className="profileImagePic"
+        />
+      </div>
+      <div className="sectionOneWhite"></div>
+      <div className="sectionOneGrey"></div>
+
+      {/* <div className="sectionOneBottomColour"></div> */}
+    </div>
+  );
+}
+
+export default SectionOne;
