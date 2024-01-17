81f3688f2fa49f78f197d91f55318a87edf5a24b
bondcab <136003551+bondcab@users.noreply.github.com>
2024-01-17 20:49:54 +0000

    Create SectionFour.jsx

---
+++ src/Components/SectionFour.jsx
@@ -0,0 +1,38 @@
+function SectionFour() {
+  return (
+    <div className="contactPageContainer">
+      <div className="contactInfoContainer">
+        <h2 className="contact">Contact</h2>
+
+        <p className="contactInfoText">
+          <span className="bold">Phone:</span>{" "}
+          <span className="hidden">...</span>07787541167
+        </p>
+        <p className="contactInfoText">
+          <span className="bold">Email:</span>{" "}
+          <span className="hidden">...</span>
+          cbond.email@gmail.com
+        </p>
+        <p className="contactInfoText">
+          <span className="bold">LinkedIn:</span>{" "}
+          <span className="hidden">...</span>
+          https://www.linkedin.com/in/cabond/
+        </p>
+        <p className="contactInfoText">
+          <span className="bold">GitHub:</span>{" "}
+          <span className="hidden">...</span>
+          <a href="https://github.com/bondcab" target="_blank" rel="noreferrer">
+            github.com/bondcab
+          </a>
+        </p>
+      </div>
+      <div className="downloadContainer">
+        <a href="resume.pdf" download>
+          <button className="downloadButton">Download my CV</button>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default SectionFour;
