82b65cab579d8f371ccf7dea1da380656d6a77bd
bondcab <136003551+bondcab@users.noreply.github.com>
2024-01-19 15:21:51 +0000

    Update SectionFour.jsx

--- src/Components/SectionFour.jsx
+++ src/Components/SectionFour.jsx
@@ -37,46 +37,57 @@ function SectionFour({ setAbout, setHome, setContact, setWork }) {
       id="#sectionFour"
     >
       <div className="contactInfoContainer">
-        <h2 className="contact" ref={domRefContact}>
-          Contact
-        </h2>
+        <div className="infoContainer">
+          <h2 className="contact" ref={domRefContact}>
+            Contact
+          </h2>
 
-        <p className="contactInfoText">
-          <span className="bold">Phone:</span>{" "}
-          <span className="hidden">...</span>07787541167
-        </p>
-        <p className="contactInfoText">
-          <span className="bold">Email:</span>{" "}
-          <span className="hidden">...</span>
-          cbond.email@gmail.com
-        </p>
-        <p className="contactInfoText">
-          <span className="bold">LinkedIn:</span>{" "}
-          <span className="hidden">...</span>
-          <a href="https://www.linkedin.com/in/cabond/">
-            <img
-              src="linkedin.svg"
-              alt="linkedin-logo"
-              className="linkedinLogoContact"
-            />
-          </a>
-        </p>
-        <p className="contactInfoText">
-          <span className="bold">GitHub:</span>{" "}
-          <span className="hidden">...</span>
-          <a href="https://github.com/bondcab">
-            <img
-              src="github.svg"
-              alt="github-logo"
-              className="githubLogoContact"
-            />
-          </a>
-        </p>
+          <p className="contactInfoText">
+            <span className="bold">Phone:</span>{" "}
+            <span className="hidden">...</span>07787541167
+          </p>
+          <p className="contactInfoText">
+            <span className="bold">Email:</span>{" "}
+            <span className="hidden">...</span>
+            cbond.email@gmail.com
+          </p>
+          <p className="contactInfoText">
+            <span className="bold">LinkedIn:</span>{" "}
+            <span className="hidden">...</span>
+            <a href="https://www.linkedin.com/in/cabond/">
+              <img
+                src="linkedin.svg"
+                alt="linkedin-logo"
+                className="linkedinLogoContact"
+              />
+            </a>
+          </p>
+          <p className="contactInfoText">
+            <span className="bold">GitHub:</span>{" "}
+            <span className="hidden">...</span>
+            <a href="https://github.com/bondcab">
+              <img
+                src="github.svg"
+                alt="github-logo"
+                className="githubLogoContact"
+              />
+            </a>
+          </p>
+        </div>
       </div>
+
       <div className="downloadContainer">
         <a href="resume.pdf" download>
           <button className="downloadButton">Download my CV</button>
         </a>
+      </div>
+      <div className="contactColourContainers">
+        <div className="contactWhiteOne"></div>
+        <div className="contactWhiteTwo"></div>
+        <div className="greyContainer">
+          <div className="contactGreyOne"></div>
+          <div className="contactGreyTwo"></div>
+        </div>
       </div>
     </div>
   );
